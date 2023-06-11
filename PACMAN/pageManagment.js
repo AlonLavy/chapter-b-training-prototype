cc3010919695bfbd2189cf94eb80787f4904e3bb
AlonLavy <alon.lavy2@gmail.com>
2023-06-11 17:25:17 +0300

    While working on validation functions.

--- PACMAN/pageManagment.js
+++ PACMAN/pageManagment.js
@@ -4,3 +4,30 @@ function switch_pages(show, hide)
     document.getElementById(hide).style.display='none';
     return false;
 }
+
+function validate_signup(username, name, password, date, email)
+{
+
+}
+
+function validate_login(username, password)
+{
+
+}
+
+function validate_date(date)
+{
+    const today = new Date();
+    return !(date > today)
+}
+
+function validate_password(password)
+{
+    return password.length >=8;
+}
+
+function validate_username(username)
+{
+    return username.length >=8
+}
+
