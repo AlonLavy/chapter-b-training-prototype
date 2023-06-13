21ddd99fefc96c59e05676287d18d3a484e09a06
AlonLavy <alon.lavy2@gmail.com>
2023-06-13 15:23:21 +0300

    enumming done.

--- PACMAN/game.js
+++ PACMAN/game.js
@@ -9,13 +9,14 @@ var interval;
 
 const boardFeatures = {
 	boardLength: 10,
-	foodRemaining: 50,
 	pacmans: 1,
 	empty: 0,
-	pacman: 2,
+	pacman: 2,	
 	food: 1,
 	obstacle: 4
 }
+
+const foodStart = (boardFeatures.boardLength**2)/2
 
 const pacmanRotation = {
 	right: 0,
@@ -41,7 +42,6 @@ function Start() {
 	const obstacles = [[3, 3], [3, 4], [3, 5], [6, 1], [6, 2]];
 	for (let i = 0; i < boardFeatures.boardLength; i++) {
 		board[i] = new Array();
-		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
 		for (let j = 0; j < boardFeatures.boardLength; j++) {
 			board[i][j] = boardFeatures.empty;
 			for (let k = 0; k < obstacles.length; k++) {
@@ -57,7 +57,7 @@ function Start() {
 		shape.j = emptyCell[1];
 		board[shape.i][shape.j] = boardFeatures.pacman;
 	}
-	for (let i = 0; i < boardFeatures.foodRemaining; i++) {
+	for (let i = 0; i < foodStart; i++) {
 		let emptyCell = findRandomEmptyCell(board);
 		board[emptyCell[0]][emptyCell[1]] = boardFeatures.food;
 	}
@@ -73,11 +73,11 @@ function Start() {
 
 
 function findRandomEmptyCell(board) {
-	let i = Math.floor((Math.random() * 9) + 1);
-	let j = Math.floor((Math.random() * 9) + 1);
+	let i = Math.floor((Math.random() * (boardFeatures.boardLength - 1)) + 1);
+	let j = Math.floor((Math.random() * (boardFeatures.boardLength - 1)) + 1);
 	while (board[i][j] !== boardFeatures.empty) {
-		i = Math.floor((Math.random() * 9) + 1);
-		j = Math.floor((Math.random() * 9) + 1);
+		i = Math.floor((Math.random() * (boardFeatures.boardLength - 1)) + 1);
+		j = Math.floor((Math.random() * (boardFeatures.boardLength - 1)) + 1);
 	}
 	return [i, j];
 }
@@ -114,12 +114,12 @@ function Draw(rotation) {
 	context.clearRect(0, 0, canvas.width, canvas.height); //clean board
 	lblScore.value = score;
 	lblTime.value = timeElapsed;
-	for (let i = 0; i < 10; i++) {
-		for (let j = 0; j < 10; j++) {
+	for (let i = 0; i < boardFeatures.boardLength; i++) {
+		for (let j = 0; j < boardFeatures.boardLength; j++) {
 			var center = new Object();
 			center.x = i * 60 + 30;
 			center.y = j * 60 + 30;
-			if (board[i][j] === 2) {
+			if (board[i][j] === boardFeatures.pacman) {
 				context.beginPath();
 				context.arc(center.x, center.y, 30, (0.15 * Math.PI) + rotation, (1.85 * Math.PI) + rotation); // half circle
 				context.lineTo(center.x, center.y);
@@ -127,17 +127,17 @@ function Draw(rotation) {
 				context.fill();
 				context.beginPath();
 				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
+				context.fillStyle = "rgb(0,0,0)"; //color
 				context.fill();
-			} else if (board[i][j] === 1) {
+			} else if (board[i][j] === boardFeatures.food) {
 				context.beginPath();
 				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
+				context.fillStyle = "rgb(0,0,0)"; //color
 				context.fill();
-			} else if (board[i][j] === 4) {
+			} else if (board[i][j] === boardFeatures.obstacle) {
 				context.beginPath();
 				context.rect(center.x - 30, center.y - 30, 60, 60);
-				context.fillStyle = "grey"; //color
+				context.fillStyle = "rgb(128,128,128)"; //color
 				context.fill();
 			}
 		}
@@ -146,31 +146,33 @@ function Draw(rotation) {
 
 function UpdatePosition() {
 	board[shape.i][shape.j] = 0;
-	var x = GetKeyPressed();
-	if (x === 1) {
-		if (shape.j > 0 && board[shape.i][shape.j - 1] !== 4) {
-			shape.j--;
-		}
+	let pressedKey = GetKeyPressed();
+	switch (pressedKey) {
+		case moveDirection.up:
+			if (shape.j > 0 && board[shape.i][shape.j - 1] !== boardFeatures.obstacle) {
+				shape.j--;
+			}
+			break;
+		case moveDirection.down:
+			if (shape.j < boardFeatures.boardLength - 1 && board[shape.i][shape.j + 1] !== boardFeatures.obstacle) {
+				shape.j++;
+			}
+			break;
+		case moveDirection.left:
+			if (shape.i > 0 && board[shape.i - 1][shape.j] !== boardFeatures.obstacle) {
+				shape.i--;
+			}
+			break;
+		case moveDirection.right:
+			if (shape.i < boardFeatures.boardLength - 1 && board[shape.i + 1][shape.j] !== boardFeatures.obstacle) {
+				shape.i++;
+			}
+			break;
 	}
-	if (x === 2) {
-		if (shape.j < 9 && board[shape.i][shape.j + 1] !== 4) {
-			shape.j++;
-		}
-	}
-	if (x === 3) {
-		if (shape.i > 0 && board[shape.i - 1][shape.j] !== 4) {
-			shape.i--;
-		}
-	}
-	if (x === 4) {
-		if (shape.i < 9 && board[shape.i + 1][shape.j] !== 4) {
-			shape.i++;
-		}
-	}
-	if (board[shape.i][shape.j] === 1) {
+	if (board[shape.i][shape.j] === boardFeatures.food) {
 		score++;
 	}
-	board[shape.i][shape.j] = 2;
+	board[shape.i][shape.j] = boardFeatures.pacman;
 	let currentTime = new Date();
 	if (startTime != 0) {
 		timeElapsed = (currentTime - startTime) / 1000;
@@ -178,14 +180,14 @@ function UpdatePosition() {
 	else {
 		timeElapsed = 0;
 	}
-	if (score >= boardFeatures.foodRemaining/2 && timeElapsed <= 10) {
+	if (score >= foodStart / 2 && timeElapsed <= 5) {
 		pac_color = "rgb(0,255,0)";
 	}
-	if (score === 50) {
+	if (score === foodStart) {
 		window.clearInterval(interval);
 		window.alert("Game completed");
 	} else {
-		switch (x) {
+		switch (pressedKey) {
 			case moveDirection.up:
 				Draw(pacmanRotation.up);
 				break;
