9de3ebb974d116f78cc83a74479ef7388ba3abfb
AlonLavy <alon.lavy2@gmail.com>
2023-06-16 13:12:31 +0300

    placing and drawing board class functions are done

--- PACMAN-OOP/modules/board.js
+++ PACMAN-OOP/modules/board.js
@@ -1,22 +1,67 @@
-import { boardItems } from './CONSTANTS';
+import * as CONSTANTS from './CONSTANTS.js';
 
 class Board {
-    constructor(boardLength) {
+    constructor(pacmans, foods, ghosts, obstacles) {
         this.board = []
-        for (let i = 0; i < boardLength; i++) {
+        for (let i = 0; i < CONSTANTS.boardItems.boardLength; i++) {
             this.board[i] = new Array();
-            for (let j = 0; j < boardLength; j++) {
-                this.board[i][j] = boardItems.empty;
+            for (let j = 0; j < CONSTANTS.boardItems.boardLength; j++) {
+                this.board[i][j] = CONSTANTS.boardItems.empty;
             }
+        }
+        this.pacmans = pacmans;
+        this.foods = foods;
+        this.ghosts = ghosts;
+        this.obstacles = obstacles;
+    }
+
+    #placeFoods() {
+        for (let i = 0; i < this.foods.length; i++) {
+            this.board[this.foods[i].location[0]][this.foods[i].location[0]] = this.foods[i];
         }
     }
 
-    placeItems(pacmans, foods, ghosts, obstacles) {
+    #placePacmans() {
+        for (let i = 0; i < this.pacmans.length; i++) {
+            this.board[this.pacmans[i].location[0]][this.pacmans[i].location[0]] = this.pacmans[i];
+        }
+    }
+
+    #placeGhosts() {
+        for (let i = 0; i < this.ghosts.length; i++) {
+            this.board[this.ghosts[i].location[0]][this.ghosts[i].location[0]] = this.ghosts[i];
+        }
+    }
+
+    #placeObsacles() {
+        for (let i = 0; i < this.obstacles.length; i++) {
+            this.board[this.obstacles[i].location[0]][this.obstacles[i].location[0]] = this.obstacles[i];
+        }
+    }
+
+
+    #placeItems() {
+        // Order of placement: food, pacman, ghosts, obstacles
+        this.#placePacmans();
+        this.#placeFoods();
+        this.#placeGhosts();
+        this.#placeObsacles();
+    }
+
+    // Draw function for each item should get center as well.
+    draw() {
+        this.#placeItems();
         let center = new Object();
         center.x = i * 60 + 30;
         center.y = j * 60 + 30;
-        // Draw function for each item should get center as well.
-        // Order of placement: food, pacman, ghosts, obstacles
 
+        for (let i = 0; i < CONSTANTS.boardItems.boardLength; i++) {
+            for (let j = 0; i < CONSTANTS.boardItems.boardLength; j++) {
+                if (this.board[i][j] != CONSTANTS.boardItems.empty)
+                {
+                    this.board[i][j].draw(center);
+                }
+            }
+        }
     }
 }
