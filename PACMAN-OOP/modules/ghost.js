a56eeac7c38970a6fee014b0f8e1754e6603a6b5
AlonLavy <alon.lavy2@gmail.com>
2023-06-17 13:41:54 +0300

    Trying to make findAllValidDirections bugless, probably there are stil errors using every and some functions

--- PACMAN-OOP/modules/ghost.js
+++ PACMAN-OOP/modules/ghost.js
@@ -19,21 +19,21 @@ export class Ghost extends BoardItem {
 
     #findAllValidDirections(board) {
         let validDirections = [];
+        const sameLocation = (ghost, direction) => ghost.location == [this.location[0] + direction[0], this.location[1] +direction[1]];
 
-        if (this.location[0] > 0 && board[this.location[0] - 1][this.location[1]].getClassName() != "Obstacle" && board[this.location[0] - 1][this.location[1]].getClassName() != "Ghost") {
+        if (this.location[0] > 0 && board[this.location[0] - 1][this.location[1]].getClassName() != "Obstacle" && !board.ghosts.some(sameLocation([-1, 0]))) {
             validDirections.push([-1, 0]); // Left
         }
-        if (ghost[0] < CONSTANTS.boardItems.boardLength - 1 && board[this.location[0] - 1][this.location[1]].getClassName() != "Obstacle" && board[this.location[0] - 1][this.location[1]].getClassName() != "Ghost") {
+        if (this.location[0] < CONSTANTS.boardItems.boardLength - 1 && board[this.location[0] + 1][this.location[1]].getClassName() != "Obstacle" && !board.ghosts.some(sameLocation([1,0]))) {
             validDirections.push([1, 0]); // Right
         }
-        if (ghost[1] > 0 && board[this.location[0]][this.location[1] - 1].getClassName() != "Obstacle" && board[this.location[0]][this.location[1] - 1].getClassName() != "Ghost") {
+        if (this.location[1] > 0 && board[this.location[0]][this.location[1] - 1].getClassName() != "Obstacle" && !board.ghosts.some(sameLocation([0, -1]))) {
             validDirections.push([0, -1]); // Up
         }
-        if (ghost[1] < CONSTANTS.boardItems.boardLength - 1 && board[this.location[0] - 1][this.location[1] + 1].getClassName() != "Obstacle" && board[this.location[0]][this.location[1] + 1].getClassName() != "Ghost") {
+        if (this.location[1] < CONSTANTS.boardItems.boardLength - 1 && board[this.location[0]][this.location[1] + 1].getClassName() != "Obstacle" && !board.ghosts.some(sameLocation([0, 1]))) {
             validDirections.push([0, 1]); // Down
         }
         if (validDirections.length == 0) {
-
             return [[0, 0]];
         }
         return validDirections;
