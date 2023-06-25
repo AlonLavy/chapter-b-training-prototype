01e981aa1d8d2eda94e9fe570601bc4212b7e814
AlonLavy <alon.lavy2@gmail.com>
2023-06-25 14:31:50 +0300

    Scoring works well now, but there is a bug where pacman stays in his last location if a need food spawns on it

--- food.js
+++ food.js
@@ -9,6 +9,11 @@ export class Food extends BoardItem {
 
     draw(context, keysDown) {
         context.beginPath();
+        context.rect(this.center.x - 30, this.center.y - 30, 60, 60);
+        context.fillStyle = CONSTANTS.colorPalette.backgroundColor; //color
+        context.fill();
+
+        context.beginPath();
         context.arc(this.center.x, this.center.y, 15, 0, 2 * Math.PI); // circle
         context.fillStyle = this.color; //color
         context.fill();
